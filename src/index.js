25faff19886995353b30e1fbfc1d0ddea95ef703
mofath <mohamed.elgaedi@gmail.com>
2022-06-29 03:03:59 +0200

    wip

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/components/App';
+import 'normalize.css';
+import './scss/index.scss';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
